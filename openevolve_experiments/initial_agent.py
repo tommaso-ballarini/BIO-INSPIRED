3faa16363f6bc3dfb0c3d6ed48b0779388f08ee7
Tommaso Ballarini <tommaso.ballarini-1@student.unitn.it>
2025-11-20 13:27:51 +0100

    openevolve prompt and initial input updated

--- openevolve_experiments/initial_agent.py
+++ openevolve_experiments/initial_agent.py
@@ -1,6 +1,12 @@
 import numpy as np
 
-def get_action(observation):    
-    # The current logic is "dumb": it chooses a random action.
-    # Modify this part.
-    return np.random.choice([0, 1, 2])
+def get_action(observation):
+    """
+    Strategy: Go UP, but try to look at RAM.
+    """
+    # Let's look at a random byte to 'simulate' sensing
+    sensor = observation[14] 
+    
+    # Currently ignoring the sensor and just going UP (1)
+    # TODO: The AI needs to change this logic to use 'sensor'
+    return 1
